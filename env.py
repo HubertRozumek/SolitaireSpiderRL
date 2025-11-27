d3e959e63cdf10616eb07d543047e8ab67d03332
Hubert Rozumek <hubert.rozumek9@gmail.com>
2025-11-27 17:26:58 +0100

    add gym environment

---
+++ env.py
@@ -0,0 +1,67 @@
+import gymnasium as gym
+from gymnasium import spaces
+import numpy as np
+from logic import Logic, NUM_COLUMNS
+
+class Env(gym.Env):
+
+    def __init__(self, render_mode=None):
+        super(Env, self).__init__()
+        self.game = Logic()
+        self.render_mode = render_mode
+
+        self.action_space = spaces.Discrete(101)
+
+        self.max_rows = 20
+        self.observation_space = spaces.Box(
+            low=0, high=13,
+            shape = (NUM_COLUMNS, self.max_rows, 2),
+            dtype=np.float32
+        )
+
+    def reset(self, seed=None, options=None):
+        super().reset(seed=seed)
+        self.game.reset()
+        return self._get_obs(), {}
+
+    def step(self, action):
+        if action == 100:
+            move = (-1, -1)
+            action_desc = "Deal"
+        else:
+            src = action // 10
+            tgt = action % 10
+            move = (src, tgt)
+            action_desc = f"{src}->{tgt}"
+
+        reward = self.game.apply_action(move)
+        reward -= 0.1
+
+        terminated = False
+        if self.game.foundation_count == 8:
+            terminated = True
+            reward += 1000
+
+        observation = self._get_obs()
+
+        if self.render_mode == 'console':
+            print(f"Action: {action_desc} | Reward: {reward} | Sets: {self.game.foundation_count}")
+
+        return observation, reward, terminated, False, {}
+
+    def _get_obs(self):
+
+        obs = np.zeros((NUM_COLUMNS, self.max_rows, 2), dtype=np.float32)
+
+        for i, col in enumerate(self.game.columns):
+            for j, card in enumerate(col):
+                if j >= self.max_rows: break
+
+                if card.face_up:
+                    obs[i,j,0] = card.rank
+                    obs[i,j,1] = 1.0
+                else:
+                    obs[i,j,0] = 0.0
+                    obs[i,j,1] = 0.0
+
+        return obs
